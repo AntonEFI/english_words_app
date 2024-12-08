8313228e33e1a999ec96591b0eed472bc003726a
AntonEFI <109530075+AntonEFI@users.noreply.github.com>
2024-12-08 23:38:13 +0500

    three_Commit

--- app/src/main/java/com/example/englishwordapp/MainActivity.kt
+++ app/src/main/java/com/example/englishwordapp/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.englishwordapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.englishwordapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding have null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            ibCloseButton.isVisible = false
+            tvGivenWord.text = "fksjdf"
+        }//урок 8
+
     }
 }
