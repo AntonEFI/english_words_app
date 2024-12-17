e48fc69386d0439c119bb4ed80c339298f1acf70
AntonEFI <109530075+AntonEFI@users.noreply.github.com>
2024-12-17 23:58:41 +0500

    six_Commit work with right and wrong ansor

--- app/src/main/java/com/example/englishwordapp/MainActivity.kt
+++ app/src/main/java/com/example/englishwordapp/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.englishwordapp
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.children
-import androidx.core.view.get
-import androidx.core.view.isVisible
 import com.example.englishwordapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +26,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        //TODO Всё ещё упростить этот код. и прятать или показывать Correct и вот всё это .по умолчанию SKIP
+        //TODO прятать или показывать Correct и вот всё это .по умолчанию SKIP
 
         with(binding){
+
+            val correctBlock: View = clCorrectBlock
+
+            correctBlock.visibility = View.GONE
+
             for (i in 0 until llWordsList.childCount){
 
                 if (i == 3){//Если правильный индекс
@@ -39,15 +42,13 @@ class MainActivity : AppCompatActivity() {
                     val ll: LinearLayout = v as LinearLayout
                     ll.setOnClickListener {
 
-                        val num: View? = ll.getChildAt(0)
+                        btnSkipButton.visibility = View.INVISIBLE//Прячем кнопку SKIP
 
-                        val number: TextView = num as TextView
+                        val number: TextView = ll.getChildAt(0) as TextView
 
-                        val text: View? = ll.getChildAt(1)
+                        val word: TextView = ll.getChildAt(1) as TextView
 
-                        val word: TextView = text as TextView
-
-                        markAnswerCorrect(ll, number, word)
+                        markAnswerCorrect(ll, number, word, correctBlock)
                     }
                 }
                 else{
@@ -56,25 +57,22 @@ class MainActivity : AppCompatActivity() {
                     val ll: LinearLayout = v as LinearLayout
 
                     ll.setOnClickListener{
-//
-                        val num: View? = ll.getChildAt(0)
 
-                        val number: TextView = num as TextView
+                        btnSkipButton.visibility = View.INVISIBLE//Прячем кнопку SKIP
 
-                        val text: View? = ll.getChildAt(1)
+                        val number: TextView = ll.getChildAt(0) as TextView
 
-                        val word: TextView = text as TextView
+                        val word: TextView = ll.getChildAt(1) as TextView
 
-                        markAnswerUncorrect(ll, number, word)
+                        markAnswerUncorrect(ll, number, word, correctBlock)
                     }
                 }
             }
         }
         //Нейтральный
     }
-    //В любом случае мне нужно в методы отправлять два TextView и менять им стили
-    //Сейчас получаеться отстой потому-что в случае верного ответа. Мне нужно переключать стили
-    private fun markAnswerCorrect(linerLayout: LinearLayout, number:TextView, word: TextView) { //Корректный ответ
+
+    private fun markAnswerCorrect(linerLayout: LinearLayout, number:TextView, word: TextView, correctBlock: View) { //Корректный ответ
 
         linerLayout.background = resources.getDrawable(R.drawable.right_ansor_values_shape)
         number.background = resources.getDrawable(R.drawable.right_ansor_squer)
@@ -82,17 +80,28 @@ class MainActivity : AppCompatActivity() {
         number.setTextColor(ContextCompat.getColor(this, R.color.white))
         word.setTextColor(ContextCompat.getColor(this, R.color.correct_ansor))
 
+        correctBlock.visibility = View.VISIBLE//SKIP нужно скрывать
+
         Log.d("Correct", "This right ansor")
     }
 
-    //Сейчас получаеться отстой потому-что в случае неверного ответа. Мне нужно переключать стили
-    private fun markAnswerUncorrect(linerLayout: LinearLayout, number:TextView, word: TextView){ //Неверный ответ
+    private fun markAnswerUncorrect(linerLayout: LinearLayout, number:TextView, word: TextView, correctBlock: View){ //Неверный ответ
 
         linerLayout.background = resources.getDrawable(R.drawable.wrong_ansor_values_shape)
         number.background = resources.getDrawable(R.drawable.wrong_ansor_squer)
 
         number.setTextColor(ContextCompat.getColor(this, R.color.white))
         word.setTextColor(ContextCompat.getColor(this, R.color.wrong_ansor_color_text))
+
+        correctBlock.visibility = View.VISIBLE
+
+        //Там из-за того, что src нельзя так просто поменять
+        binding.ivIcCorrect.background = resources.getDrawable(R.drawable.ic_wrong__1)
+
+        binding.tvResult.text = resources.getText(R.string.wrong)
+        binding.btnCorrectButton.setTextColor(ContextCompat.getColor(this, R.color.wrong_ansor_color_text))
+
+        correctBlock.setBackgroundColor(ContextCompat.getColor(this, R.color.wrong_ansor))
 
         Log.d("Uncorrect", "This not right ansor")
     }
